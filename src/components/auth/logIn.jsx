8c2dcf5191221b6dd84b1ae45432adbc2b4151d5
unknown <hansi20ra@example.com>
2023-08-17 21:51:36 +0530

    change login

--- src/components/auth/logIn.jsx
+++ src/components/auth/logIn.jsx
@@ -1,87 +1,108 @@
-import {useState} from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./signUp.css";
-import {useNavigate} from "react-router-dom";
+import "./logIn.css";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-const navigate=useNavigate()
-   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:5000/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await fetch("http://localhost:5000/users/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
 
-       const data = await response.json();
-      if(data.token!=null) localStorage.setItem('token', data.token);
+			const data = await response.json();
+			if (data.token != null) localStorage.setItem("token", data.token);
 
-      if (response.ok) {
-        navigate("/home")
-        
-        console.log('Authentication successful');
-      } else {
-        
-        console.log('Authentication failed');
-      }
-    } catch (error) {
-      // Handle any network or other errors
-      console.error('Error occurred while calling the API', error);
-    }
-  };
-  return (
-    <div className='auth_container'>
-      <form className='form-container'onSubmit={handleSubmit}>
-        <h1 className='h1-signUp'>LogIn</h1>
+			if (response.ok) {
+				navigate("/home");
 
-        {/* Email input */}
-        <div className='form-outline mb-4'>
-          <input type='email' id='form3Example3' className='form-control' name={"email"} value={formData.email} onChange={handleChange}/>
-          <label className='form-label' htmlFor='form3Example3'>
-            Email address
-          </label>
-        </div>
+				console.log("Authentication successful");
+			} else {
+				console.log("Authentication failed");
+			}
+		} catch (error) {
+			// Handle any network or other errors
+			console.error("Error occurred while calling the API", error);
+		}
+	};
+	return (
+		<div className='grid_container'>
+			<div className='pic_container'>
+				<h1>hi baba</h1>
+			</div>
 
-        {/* Password input */}
-        <div className='form-outline mb-4'>
-          <input type='password' id='form3Example4' className='form-control' name={"password"} value={formData.password} onChange={handleChange}/>
-          <label className='form-label' htmlFor='form3Example4'>
-            Password
-          </label>
-        </div>
+			<div className='login_container '>
+				<form
+					className='login-form'
+					onSubmit={handleSubmit}>
+					<h1 className='h1-login'>LogIn</h1>
 
-        {/* Submit button */}
-        <button
-          type='submit'
-          className='btn signup-btn mb-4 me-4'
-         
-        >
-          Log In
-        </button>
+					{/* Email input */}
+					<div className='loginform-outline mb-4'>
+						<input
+							type='email'
+							id='form3Example3'
+							className='loginform-control'
+							name={"email"}
+							value={formData.email}
+							onChange={handleChange}
+						/>
+						<label
+							className='loginform-label'
+							htmlFor='form3Example3'>
+							Email address
+						</label>
+					</div>
 
-        <button
-         
-          className='btn signup-btn mb-4'
-          onClick={() => navigate("/signUp")}
-        >
-          Sign up
-        </button>
-      </form>
-    </div>
-  );
+					{/* Password input */}
+					<div className='loginform-outline mb-4'>
+						<input
+							type='password'
+							id='form3Example4'
+							className='loginform-control'
+							name={"password"}
+							value={formData.password}
+							onChange={handleChange}
+						/>
+						<label
+							className='loginform-label'
+							htmlFor='form3Example4'>
+							Password
+						</label>
+					</div>
+
+					{/* Submit button */}
+					<button
+						type='submit'
+						className='btn login-btn mb-4 me-4'>
+						Log In
+					</button>
+
+					<button
+						className='btn signup-btn mb-4'
+						onClick={() => navigate("/signUp")}>
+						Sign up
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default LoginForm;
--- src/components/auth/signUp.jsx
+++ src/components/auth/signUp.jsx
@@ -1,93 +1,117 @@
-import  {useState} from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./signUp.css";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const SignupForm = () => {
-  const navigate=useNavigate()
-   const [formData, setFormData] = useState({
-    name:'',
-    email: '',
-    password: '',
-  });
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('http://localhost:5000/users/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-    const data=await response.json()
-    console.log(data.message);
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await fetch("http://localhost:5000/users/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await response.json();
+			console.log(data.message);
 
-      if (response.ok) {
-       navigate('/home')
-        console.log(data.message);
-      } else {
-        // Handle authentication error here, e.g., show error message to the user
-        console.log(data.error);
-      }
-    } catch (error) {
-      // Handle any network or other errors
-      console.error('Error occurred while calling the API', error);
-    }
-  };
+			if (response.ok) {
+				navigate("/home");
+				console.log(data.message);
+			} else {
+				// Handle authentication error here, e.g., show error message to the user
+				console.log(data.error);
+			}
+		} catch (error) {
+			// Handle any network or other errors
+			console.error("Error occurred while calling the API", error);
+		}
+	};
 
+	return (
+		<div
+			className='auth_container'
+			onSubmit={handleSubmit}>
+			<form className='form-container'>
+				<h1 className='h1-signUp'>SignUp</h1>
+				{/* 2 column grid layout with text inputs for the first and last names */}
+				<div className='row mb-4'>
+					<div className='col'>
+						<div className='form-outline'>
+							<input
+								type='text'
+								id='form3Example1'
+								className='form-control'
+								name={"name"}
+								value={formData.name}
+								onChange={handleChange}
+							/>
+							<label
+								className='form-label'
+								htmlFor='form3Example1'>
+								First name
+							</label>
+						</div>
+					</div>
+				</div>
 
-  return (
-    <div className='auth_container'onSubmit={handleSubmit}>
-      <form className='form-container'>
-        <h1 className='h1-signUp'>SignUp</h1>
-        {/* 2 column grid layout with text inputs for the first and last names */}
-        <div className='row mb-4'>
-          <div className='col'>
-            <div className='form-outline'>
-              <input type='text' id='form3Example1' className='form-control' name={"name"} value={formData.name} onChange={handleChange} />
-              <label className='form-label' htmlFor='form3Example1'>
-                First name
-              </label>
-            </div>
-          </div>
-          
-        </div>
+				{/* Email input */}
+				<div className='form-outline mb-4'>
+					<input
+						type='email'
+						id='form3Example3'
+						className='form-control'
+						name={"email"}
+						value={formData.email}
+						onChange={handleChange}
+					/>
+					<label
+						className='form-label'
+						htmlFor='form3Example3'>
+						Email address
+					</label>
+				</div>
 
-        {/* Email input */}
-        <div className='form-outline mb-4'>
-          <input type='email' id='form3Example3' className='form-control' name={"email"} value={formData.email} onChange={handleChange} />
-          <label className='form-label' htmlFor='form3Example3'>
-            Email address
-          </label>
-        </div>
+				{/* Password input */}
+				<div className='form-outline mb-4'>
+					<input
+						type='password'
+						id='form3Example4'
+						className='form-control'
+						name={"password"}
+						value={formData.password}
+						onChange={handleChange}
+					/>
+					<label
+						className='form-label'
+						htmlFor='form3Example4'>
+						Password
+					</label>
+				</div>
 
-        {/* Password input */}
-        <div className='form-outline mb-4'>
-          <input type='password' id='form3Example4' className='form-control' name={"password"} value={formData.password} onChange={handleChange} />
-          <label className='form-label' htmlFor='form3Example4'>
-            Password
-          </label>
-        </div>
-
-        {/* Submit button */}
-        <button
-          type='submit'
-          className='btn signup-btn mb-4'
-          
-        >
-          Sign up
-        </button>
-      </form>
-    </div>
-  );
+				{/* Submit button */}
+				<button
+					type='submit'
+					className='btn signup-btn mb-4'>
+					Sign up
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default SignupForm;
