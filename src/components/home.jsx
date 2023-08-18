0fed49cb6649d3a97cd5013ece5cc34279b1a588
unknown <hansi20ra@example.com>
2023-08-18 13:35:55 +0530

    add styles for home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,48 +1,57 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "./home/searchBar";
-import HorizontalDropdowns from "./home/dropdownList"
-import CardGrid from "./home/cardGrid"
+import HorizontalDropdowns from "./home/dropdownList";
+import CardGrid from "./home/cardGrid";
 
 function Home() {
-async function GetItems(data){
-
-const baseUrl="http://127.0.0.1:8000/predict/"
-const {text="i want to participate to wedding at night",color="white",
-type="formal",categories="t-shirt",gender="male"}={text:"i want to participate to wedding at night",color:"white",
-type:"formal",categories:"t-shirt",gender:"male"};
-console.log(text,type,gender,categories);
-  	try {
-			const response = await fetch( `${baseUrl}/${text}/${color}/${gender}/${type}/${categories}`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				
-			});
+	async function GetItems(data) {
+		const baseUrl = "http://127.0.0.1:8000/predict/";
+		const {
+			text = "i want to participate to wedding at night",
+			color = "white",
+			type = "formal",
+			categories = "t-shirt",
+			gender = "male",
+		} = {
+			text: "i want to participate to wedding at night",
+			color: "white",
+			type: "formal",
+			categories: "t-shirt",
+			gender: "male",
+		};
+		console.log(text, type, gender, categories);
+		try {
+			const response = await fetch(
+				`${baseUrl}/${text}/${color}/${gender}/${type}/${categories}`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
 			const data = await response.json();
 			if (data.token != null) localStorage.setItem("token", data.token);
 
 			if (response.ok) {
 				navigate("/home");
-
-				
 			} else {
-				
 			}
 		} catch (error) {
 			// Handle any network or other errors
 			console.error("Error occurred while calling the API", error);
 		}
- }
+	}
 
-  return (
-    <>
-      <SearchBar GetItems={GetItems}/>
-      <CardGrid />
-    </>
-  );
+	return (
+		<>
+			<div className='container-sm'>
+				<SearchBar GetItems={GetItems} />
+				<CardGrid />
+			</div>
+		</>
+	);
 }
 
 export default Home;
--- src/components/home/searchBar.jsx
+++ src/components/home/searchBar.jsx
@@ -1,61 +1,51 @@
-import React, {useState} from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
 import styles from "./searchBar.module.css";
 import HorizontalDropdowns from "./dropdownList";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const SearchBar = ({GetItems}) => {
-  const [userInput, setUserInput] = useState("");
- const [selectedOptions, setSelectedOptions] = useState({
-    situations: null,
-    color: null,
-    gender: null,
-    type: null,
-    categories: null,
-  });
+const SearchBar = ({ GetItems }) => {
+	const [userInput, setUserInput] = useState("");
+	const [selectedOptions, setSelectedOptions] = useState({
+		situations: null,
+		color: null,
+		gender: null,
+		type: null,
+		categories: null,
+	});
 
-  const handleInputChange = (event) => {
-    setUserInput(event.target.value);
-    
-    console.log(event.target.value);
-  
-  };
- const search=()=>{
-  const serachData={...selectedOptions,text:userInput}
-  GetItems(serachData)
- }
-  return (
-    <>
-    <div className={styles.search_container}>
-      <label htmlFor='userInput'></label>
-      <div className={styles.search_wrapper}>
-        <input
-          placeholder='Search'
-          className={styles.search_bar}
-          type='text'
-          id='userInput'
-          name='userInput'
-          value={userInput}
-          onChange={handleInputChange}
-        />
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='16'
-          height='16'
-          fill='currentColor'
-          className={styles.search_icon}
-          viewBox='0 0 16 16'
-        >
-          <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-        </svg>
-        <button onClick={()=>{        
-          search()}}>Search</button>
-      </div>
-      
+	const handleInputChange = (event) => {
+		setUserInput(event.target.value);
 
-    </div>
-    <HorizontalDropdowns  onSelect={setSelectedOptions}/>
-    </>
-  );
+		console.log(event.target.value);
+	};
+	const search = () => {
+		const serachData = { ...selectedOptions, text: userInput };
+		GetItems(serachData);
+	};
+	return (
+		<>
+			<div class={styles.input_group}>
+				<div className={styles.form_outline}>
+					<input
+						type='search'
+						id='form1'
+						className={styles.form_control}
+						placeholder='Search here...'
+					/>
+					<label
+						className={styles.form_label}
+						htmlFor='form1'></label>
+				</div>
+				<div className={styles.btn_div}>
+					<button
+						type='button'
+						className={styles.btn}>
+						<i class='fas fa-search'></i>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default SearchBar;
