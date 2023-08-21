1bf91e724b3a8795bbdd893b9ca2106014e0452d
DESKTOP-EPM5P8O\skyline <nipunchathuranga45@gmail.com>
2023-08-21 10:07:22 +0530

    change card

--- src/components/home/cardComponent.jsx
+++ src/components/home/cardComponent.jsx
@@ -1,16 +1,16 @@
-import { useNavigate,useHistory  } from "react-router-dom";
+import { useNavigate  } from "react-router-dom";
 import styles from "./cardComponent.module.css";
 
 const CardContainer = (props) => {
   const navigate=useNavigate()
-   const history = useHistory();
+  //  const history = useHistory();
   function cardOnClick(){
       navigate(`/card/${props.data}`)
-   history.push({
-      pathname: '/card',
-      state: { },
-    });
-       console.log(props.data);
+  //  history.push({
+  //     pathname: '/card',
+  //     state: { },
+  //   });
+  //      console.log(props.data);
   }
   return (
     <div className={styles.card} onClick={cardOnClick}>
